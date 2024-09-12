468c517ad150182e98cf3b8f31f60a5d579b4860
Tom Radford <tom@teamgeek.io>
2024-09-12 08:56:29 +0200

    use our `pg` sql stub

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,16 +1,10 @@
 import bcrypt from "bcrypt";
 import { invoices, customers, revenue, users } from "../lib/placeholder-data";
-import postgres from "postgres";
-
-const DB_URL = `${process.env.POSTGRES_URL}`;
-
-const client = {
-  sql: postgres(DB_URL),
-};
+import { sql } from "../../db";
 
 async function seedUsers() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await client.sql`
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`
     CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
@@ -22,7 +16,7 @@ async function seedUsers() {
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
-      return client.sql`
+      return sql`
         INSERT INTO users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
         ON CONFLICT (id) DO NOTHING;
@@ -34,9 +28,9 @@ async function seedUsers() {
 }
 
 async function seedInvoices() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  await client.sql`
+  await sql`
     CREATE TABLE IF NOT EXISTS invoices (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       customer_id UUID NOT NULL,
@@ -48,7 +42,7 @@ async function seedInvoices() {
 
   const insertedInvoices = await Promise.all(
     invoices.map(
-      (invoice) => client.sql`
+      (invoice) => sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
         ON CONFLICT (id) DO NOTHING;
@@ -60,9 +54,9 @@ async function seedInvoices() {
 }
 
 async function seedCustomers() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  await client.sql`
+  await sql`
     CREATE TABLE IF NOT EXISTS customers (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
@@ -73,7 +67,7 @@ async function seedCustomers() {
 
   const insertedCustomers = await Promise.all(
     customers.map(
-      (customer) => client.sql`
+      (customer) => sql`
         INSERT INTO customers (id, name, email, image_url)
         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
         ON CONFLICT (id) DO NOTHING;
@@ -85,7 +79,7 @@ async function seedCustomers() {
 }
 
 async function seedRevenue() {
-  await client.sql`
+  await sql`
     CREATE TABLE IF NOT EXISTS revenue (
       month VARCHAR(4) NOT NULL UNIQUE,
       revenue INT NOT NULL
@@ -94,7 +88,7 @@ async function seedRevenue() {
 
   const insertedRevenue = await Promise.all(
     revenue.map(
-      (rev) => client.sql`
+      (rev) => sql`
         INSERT INTO revenue (month, revenue)
         VALUES (${rev.month}, ${rev.revenue})
         ON CONFLICT (month) DO NOTHING;
@@ -107,16 +101,20 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    await client.sql.begin(``, () => console.log("Transaction began."));
+    // create database
+    await sql`
+      CREATE DATABASE IF NOT EXISTS nextjs-dashboard-postgres;
+    `;
+
     await seedUsers();
     await seedCustomers();
     await seedInvoices();
     await seedRevenue();
-    await client.sql`COMMIT`;
+    await sql`COMMIT`;
 
     return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
-    await client.sql`ROLLBACK`;
+    await sql`ROLLBACK`;
     return Response.json({ error }, { status: 500 });
   }
 }
